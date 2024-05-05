0050db064520c29db850b15b6d83fb5ea9ae59a8
Roman <140193303+Belxz777@users.noreply.github.com>
2024-05-05 19:47:33 +0400

    lll'

--- src/app/testsd/page.tsx
+++ src/app/testsd/page.tsx
@@ -2,61 +2,57 @@
 import React,{useState,useEffect,useRef} from 'react'
 import { useRouter } from 'next/navigation'
 import { getAllQuests } from '@/components/server/getAllQuests'
+import { fetchAllTitles } from '@/components/server/FetchJobTitle'
 type Props = {}
 
-  function Karta ({}: Props) {
-  useEffect( () => {
-/**
-* Fetches all quest data and updates the quizData state.
-* 
-* This function is responsible for retrieving all the quest data from the backend and updating the quizData state with the fetched data. If there is an error during the fetch, it will throw an error.
-*/
-const fetchData = async () => {
-const allQues = await getAllQuests()
-
-
-setQuizData(allQues)
+function Main({ }: Props) {
+const [a, seta] = useState("")
+const [state, setState] = useState({
+name: "Roman",
+description: "",
+members: [
+{
+id: 0,
+name: "",
+surname: ""
 }
-    fetchData()
-        },[])
-      const [quizData,setQuizData] = useState<any>([{
-        "id":0,
-        "question":"",
-        "answer":"",
-        "variants":["", "","",""],
-        "location": "",
-        "hardness":"",
-        "lat":0,
-        "lon": 0,
-        "author":"",
-        "quizIn": "",
-        "categorie":""
-      }])
-      
-      const [augedInfo, setaugedInfo] = useState<any>({
-        ip:0,
-        lat:56.856825,
-        lon:53.198824,
-      })
-      const router = useRouter()
-    const height = window.innerHeight - 100
-    const width = window.innerWidth
-   // const  coordinaets = [{longtail:56.856825,sovtail:53.198824,title:'Квест в падике'},{longtail:56.862081,sovtail:53.218237,title:'Квест на парусах'},]
-    
-  return (
-    <main className='  '>
-                    <button className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4 rounded-full text-xl flex" onClick={()=> router.push(`/`)}>
-Обратно на главную
+]
+})
+
+const [titles, settitles] = useState<any>()
+useEffect(() => {
+  const fetchData= async () => {
+const response  = await fetchAllTitles()
+settitles(response)
+console.log(response)
+}
+fetchData()
+}, [])
+
+const handleSubmit = () => {
+alert(state)
+}
+
+return (
+<main className='  '>
+<input
+value={state.name}
+onChange={(e) => {
+setState({ ...state, name: e.target.value })
+}} />
+<button className=" bg-button-base hover:bg-hint-base text-button-base font-bold py-2 px-4 rounded-full text-xl flex" onClick={() => handleSubmit()}>
+Оправить
 </button>
-    <div className='  w-full '>
-    {
-  quizData[0].id == 0 ? 
-<h1>неТ ДАТЫ</h1>
-  :
-      <h1>{quizData}</h1>
+{
+state.name == "Roman" ?
+<h1>Нельзя</h1>
+:
+<h1>
+  Все роман
+</h1>
 }
-</div>
 </main>
-  )
+)
 }
-export default Karta
+export default Main
+
